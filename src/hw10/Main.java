40d99ca99f91b11fb22798d4822f3c696a1a2444
belenkotv <unamunatara@mail.ru>
2021-07-29 13:18:33 +0300

    HomeWork #10

---
+++ src/hw10/Main.java
@@ -0,0 +1,35 @@
+package hw10;
+
+
+import java.util.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Task 1
+        List<String> words = Arrays.asList("яблоко", "груша", "абрикос", "банан", "мандарин", "лимон",
+                "дыня", "яблоко", "апельсин", "арбуз", "банан", "дыня", "лимон", "груша", "мандарин", "апельсин");
+        HashMap<String, Integer> uniqueWords = new HashMap<>();
+        for (String word: words) {
+            int count = uniqueWords.getOrDefault(word, 0);
+            uniqueWords.put(word, count + 1);
+        }
+        System.out.println(uniqueWords);
+
+        // Task 2
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Арбузов", "89543587634");
+        phonebook.add("Смирнов", "12321312332");
+        phonebook.add("Борисов", "47873892784");
+        phonebook.add("Громов", "41425458888");
+        phonebook.add("Чернов", "78957766677");
+        phonebook.add("Борисов", "44448887676");
+        phonebook.get("Арбузов");
+        phonebook.get("Путин");
+        phonebook.get("Борисов");
+
+    }
+
+}
---
+++ src/hw10/Phonebook.java
@@ -0,0 +1,24 @@
+package hw10;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Phonebook {
+
+    HashMap<String, HashSet<String>> dictionary = new HashMap<>();
+
+    boolean add(String name, String number) {
+        HashSet<String> numbers = dictionary.getOrDefault(name, new HashSet<>());
+        boolean ok = numbers.add(number);
+        if (ok) {
+            dictionary.put(name, numbers);
+        }
+        return ok;
+    }
+
+    void get(String name) {
+        System.out.print(name + ": ");
+        System.out.println(dictionary.getOrDefault(name, new HashSet<>()));
+    }
+
+}
